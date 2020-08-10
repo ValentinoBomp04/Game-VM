bbb23df51f9ab38643063d35bd14c669d801f4f2
Valentino-Moreno <63946957+Valentino-Moreno@users.noreply.github.com>
2020-08-10 14:21:38 -0300

    Add files via upload

---
+++ shooterV3.py
@@ -0,0 +1,62 @@
+import pygame, random
+WIDTH = 1091
+HEIGHT = 614
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+pygame.init() 
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("shooter")
+clock = pygame.time.Clock()
+fondo = pygame.image.load("fondo.png").convert()
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image =  pygame.image.load("milico.png").convert()
+        self.image.set_colorkey(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH // 2
+        self.rect.bottom = HEIGHT  - 10
+        self.rect.top = HEIGHT  + 10
+        self.speed_x = 0
+        self.speed_y = 0
+    def update(self):
+        self.speed_x = 0
+        self.speed_y = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speed_x = -10
+        if keystate[pygame.K_RIGHT]:
+            self.speed_x = 10
+        if keystate[pygame.K_UP]:
+            self.speed_y = -10
+        if keystate[pygame.K_DOWN]:
+            self.speed_y = 10
+        self.rect.x += self.speed_x
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+            
+        self.rect.y += self.speed_y
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+        if self.rect.top < 0:
+            self.rect.top = 0
+            
+            
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+running = True
+while running: 
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running == False
+            
+    all_sprites.update()
+    screen.blit(fondo, [0, 0])
+    all_sprites.draw(screen)
+    pygame.display.flip()
+pygame.quit()
